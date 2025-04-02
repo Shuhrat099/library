1d0a24be4203cf94147de46d5f2eb5aa458c5afb
Shuhrat <shuhrat@Ibrohims-iMac.local>
2025-04-02 12:20:53 +0500

    feat: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import Library from "./Library.jsx";
-import ToDoList from "./ToDoList.jsx";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Library from "./Library";
+import ToDoList from "./ToDoList";
 
 const App = () => {
     return (
-        <div>
-            {/*<Library />*/}
-            <ToDoList />
-        </div>
+        <Router>
+            <div className="p-5">
+                <ul className="flex gap-2">
+                    <li>
+                        <Link to="/library" className="cursor-pointer text-blue-600">
+                            Library
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/todolist" className="cursor-pointer text-blue-600">
+                            List
+                        </Link>
+                    </li>
+                </ul>
+
+                {/* Routing */}
+                <Routes>
+                    <Route path="/library" element={<Library />} />
+                    <Route path="/todolist" element={<ToDoList />} />
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
